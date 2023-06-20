82e127786c496a6d9d0fa01045d697daa0b5e2a5
Alex212298 <122974691+Alex212298@users.noreply.github.com>
2023-06-20 12:05:57 +0300

    Added 2 pages
    Added 2 pages for news. used jinja templates

--- app/news_paper/models.py
+++ app/news_paper/models.py
@@ -25,9 +25,14 @@ class Author (models.Model):
         self.rating = pRat * 3 + cRat
         self.save()
 
+    def __str__(self):
+        return self.authorUser
 
 class Category (models.Model):
     categoryName = models.CharField(max_length=65, unique=True)
+
+    def __str__(self):
+        return self.categoryName
 
 
 class Post (models.Model):
@@ -62,6 +67,8 @@ class Post (models.Model):
     def preview(self):
         return f"{self.text[:125]}..."
 
+    def __str__(self):
+        return self.title
 
 class PostCategory (models.Model):
     postTR = models.ForeignKey(Post, on_delete=models.CASCADE)
--- app/news_paper/views.py
+++ app/news_paper/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import *
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    extra_context = {'title': 'Главная страница'}
+    queryset = Post.objects.order_by('-dateCreate')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class PostOne(DetailView):
+    model = Post
+    template_name = 'news_detail.html'
+    context_object_name = 'one_news'
+    extra_context = {'title': 'Новость'}
